b002b935e553cf3b2cf44ac31396455f7c82bdc8
终点在爱琴海 <32701693+likangming@users.noreply.github.com>
2019-01-03 15:57:14 +0800

    Update 数据合成并统计显示出来.py

--- 数据合成并统计显示出来.py
+++ 数据合成并统计显示出来.py
@@ -1,1 +1,27 @@
+myself = {'apples': 3, 'bananas': 6, 'pear': 32, 'peach': 2, 'grape': 12}
+she = ['orange', 'jujube', 'orange', 'tomato', 'orange']
 
+
+def addtoinvendory(a, b):
+    d1 = {}
+    for i in b:
+        d1[i] = b.count(i)
+    for k, v in d1.items():
+        if k in a.keys():
+            a[k] = a[k] + v
+        else:
+            a[k] = v
+    return a
+
+
+def invendory(inven):
+    print('Invendory:')
+    total = 0
+    for k, v in inven.items():
+        print('-', k, v)
+        total = total + v
+    print('Total:', total)
+
+
+myself = addtoinvendory(myself, she)
+invendory(myself)
